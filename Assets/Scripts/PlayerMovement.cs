55ac6aa462f3328d7e8527612314654d22353f94
Ryan Wood <ryan-wood-1991@hotmail.com>
2022-05-30 07:29:16 +0100

    Main Menu - Pause Menu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float MoveSpeed = 3f;
+    public GameObject pauseMenu;
+    public bool isPaused;
     Vector2 moveInput;
     Rigidbody2D _rigidbody2D;
     Animator _animator;
@@ -13,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        pauseMenu.SetActive(false);
+        isPaused = false;
     }
 
     void Update()
@@ -35,7 +40,6 @@ public class PlayerMovement : MonoBehaviour
         _animator.SetBool("IsMoving", hasHorizontalSpeed);
     }
 
-
     void FlipSprite()
     {
         bool hasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
@@ -45,5 +49,41 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x), 1f);
         }
 
+    }
+    void OnPause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
 }
